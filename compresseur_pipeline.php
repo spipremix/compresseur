5b6d23e9026539b5667082554c2e2e39bee80a6b
Cerdic <cedric@yterium.com>
2010-01-27 22:10:09 +0000

    oups dans [34747] qui avait emporte un test utile

--- compresseur_pipeline.php
+++ compresseur_pipeline.php
@@ -6,16 +6,18 @@ function compresseur_header_prive($flux){
 }
 
 function compresseur_affiche_milieu($flux){
-
-	// dans la branche 2.1 on utilise l'ancienne interface du compresseur
-	// par homogeneite
-	if (version_compare($GLOBALS['spip_version_branche'], "2.2.0-dev","<")){
-		$compresseur = charger_fonction('compresseur', 'configuration');
-		$flux['data'] .= $compresseur(); // Compression http et compactages CSS ou JS
-	}
-	else
-		$flux['data'] .= recuperer_fond('prive/configurer/compresseur',array()); // Compression http et compactages CSS ou JS
 	
+	if ($flux['args']['exec']=='config_fonctions'){
+		// dans la branche 2.1 on utilise l'ancienne interface du compresseur
+		// par homogeneite
+		if (version_compare($GLOBALS['spip_version_branche'], "2.2.0-dev","<")){
+			$compresseur = charger_fonction('compresseur', 'configuration');
+			$flux['data'] .= $compresseur(); // Compression http et compactages CSS ou JS
+		}
+		else
+			$flux['data'] .= recuperer_fond('prive/configurer/compresseur',array()); // Compression http et compactages CSS ou JS
+	}
+
 	return $flux;
 }
 

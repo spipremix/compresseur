5b1f4ad9b08722a76d90765858aad3294a6d844f
jrynik <jrynik@yahoo.co.uk>
2011-08-05 14:18:41 +0000

    langues (compresseur)

--- lang/compresseur_sk.php
+++ lang/compresseur_sk.php
@@ -8,19 +8,19 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
-	'info_compresseur_titre' => 'Optimisations and compression', # MODIF
-	'info_question_activer_compactage_css' => 'Do you wish to activate compression for CSS stylesheets?', # MODIF
-	'info_question_activer_compactage_js' => 'Do you wish to activate compression for Javascript files?', # MODIF
-	'info_question_activer_compresseur' => 'Do you wish to activate compression for the HTTP data?', # MODIF
-	'item_compresseur' => 'Activate compression', # MODIF
-	'item_compresseur_closure' => 'Utiliser Google Closure Compiler [expérimental]', # NEW
+	'info_compresseur_titre' => 'Optimalizácia a kompresia',
+	'info_question_activer_compactage_css' => 'Chcete aktivovať kompresiu pre štýlopisy CSS?',
+	'info_question_activer_compactage_js' => 'Chcete aktivovať kompresiu pre javascriptové súbory?',
+	'info_question_activer_compresseur' => 'Chcete aktivovať kompresiu pre dáta HTTP?',
+	'item_compresseur' => 'Aktivovať kompresiu',
+	'item_compresseur_closure' => 'Používať Google Closure Compiler [experimentálny]',
 
 	// T
 	'texte_compacter_avertissement' => 'Be careful not to activate these options during the development of your site: compressed elements become difficult to read and debug.',
 	'texte_compacter_script_css' => 'SPIP can compact Javascript files and CSS stylesheets and save them as static files. This accelerates the display of the site.',
 	'texte_compresseur_page' => 'SPIP can automatically compress each page that it sends. This option reduces the bandwidth used, making the site faster for lower speed connections), but it does require more resources from the server.',
-	'titre_compacter_script_css' => 'Compression of scripts and CSS', # MODIF
-	'titre_compresser_flux_http' => 'Compression of HTTP data' # MODIF
+	'titre_compacter_script_css' => 'Kompresia skriptov a CSS',
+	'titre_compresser_flux_http' => 'Kompresia dát HTTP'
 );
 
 ?>

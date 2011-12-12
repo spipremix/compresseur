69d5c5c28ffeef79754bbce5a0bcbdac42ead222
salvatore <salvatore@rezo.net>
2011-12-12 21:13:27 +0000

    langues (compresseur)

--- lang/compresseur_en.php
+++ lang/compresseur_en.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -9,20 +9,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_compresseur_titre' => 'Optimisation and compression',
-	'info_question_activer_compactage_css' => 'Do you wish to activate compression for CSS stylesheets?', # MODIF
-	'info_question_activer_compactage_js' => 'Do you wish to activate compression for Javascript files?', # MODIF
-	'info_question_activer_compresseur' => 'Do you wish to activate compression for HTTP data?', # MODIF
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Scripts',
+	'info_question_activer_compresseur' => 'HTML',
 	'item_compresseur_closure' => 'Use Google Closure Compiler [experimental]',
-	'item_compresseur_css' => 'Activer la compression des feuilles de styles (CSS)', # NEW
-	'item_compresseur_html' => 'Activer la compression du HTML', # NEW
-	'item_compresseur_js' => 'Activer la compression des scripts (javascript)', # NEW
+	'item_compresseur_css' => 'Enable stylesheets compression (CSS)',
+	'item_compresseur_html' => 'Enable HTML compression',
+	'item_compresseur_js' => 'Enable scripts compression (javascript)',
 
 	// T
 	'texte_compacter_avertissement' => 'Be careful not to activate these options during site development, as compressed elements are difficult to read and debug.',
 	'texte_compacter_script_css' => 'SPIP can compact Javascript files and CSS stylesheets and save them as static files. This makes the site display faster on browsers.',
 	'texte_compresseur_page' => 'SPIP can automatically compress every page it sends. This option reduces bandwidth, making the site faster for lower speed connections), but it does require more resources from the server.',
 	'titre_compacter_script_css' => 'Compression of scripts and CSS',
-	'titre_compresser_flux_http' => 'Compression of HTTP data' # MODIF
+	'titre_compresser_flux_http' => 'HTML compression'
 );
 
 ?>

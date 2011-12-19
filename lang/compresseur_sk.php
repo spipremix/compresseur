221912539c8db5bda99b75aadc78e5eb1087d79e
salvatore <salvatore@rezo.net>
2011-12-19 16:09:24 +0000

    langues (compresseur)

--- lang/compresseur_sk.php
+++ lang/compresseur_sk.php
@@ -9,20 +9,21 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_compresseur_titre' => 'Optimalizácia a kompresia',
-	'info_question_activer_compactage_css' => 'Chcete aktivovať kompresiu pre štýlopisy CSS?', # MODIF
-	'info_question_activer_compactage_js' => 'Chcete aktivovať kompresiu pre javascriptové súbory?', # MODIF
-	'info_question_activer_compresseur' => 'Chcete aktivovať kompresiu pre dáta HTTP?', # MODIF
-	'item_compresseur_closure' => 'Používať Google Closure Compiler [experimentálny]',
-	'item_compresseur_css' => 'Activer la compression des feuilles de styles (CSS)', # NEW
-	'item_compresseur_html' => 'Activer la compression du HTML', # NEW
-	'item_compresseur_js' => 'Activer la compression des scripts (javascript)', # NEW
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Skripty',
+	'info_question_activer_compresseur' => 'HTML',
+	'item_compresseur_closure' => 'Používať kompilátor Google Closure [experimentálny]',
+	'item_compresseur_css' => 'Aktivovať kompresiu súborov so štýlmi(CSS)',
+	'item_compresseur_html' => 'Aktivovať kompresiu HTML',
+	'item_compresseur_js' => 'Aktivovať kompresiu skriptov
+(javascript)',
 
 	// T
-	'texte_compacter_avertissement' => 'Be careful not to activate these options during the development of your site: compressed elements become difficult to read and debug.',
-	'texte_compacter_script_css' => 'SPIP can compact Javascript files and CSS stylesheets and save them as static files. This accelerates the display of the site.',
-	'texte_compresseur_page' => 'SPIP can automatically compress each page that it sends. This option reduces the bandwidth used, making the site faster for lower speed connections), but it does require more resources from the server.',
+	'texte_compacter_avertissement' => 'Dajte pozor na to, aby ste počas vývoja svojej stránky neaktivovali tieto možnosti: kompaktné prvky sa stanú nečitateľné.',
+	'texte_compacter_script_css' => 'SPIP dokáže zmenšiť Javascriptove súbory a súbory so štýlmi a uložiť ich ako statické súbory. Tak zrýchľuje zobrazenie stránky.',
+	'texte_compresseur_page' => 'SPIP dokáže automaticky skomprimovať každú stránku, ktorú posiela návštevníkovi stránky. Tieto nastavenia optimalizujú šírku pásma (pri pomalšom pripojení je stránka rýchlejšia), ale vyžadujú si väčší výkon na strane servera. ',
 	'titre_compacter_script_css' => 'Kompresia skriptov a CSS',
-	'titre_compresser_flux_http' => 'Kompresia dát HTTP' # MODIF
+	'titre_compresser_flux_http' => 'Kompresia HTML'
 );
 
 ?>

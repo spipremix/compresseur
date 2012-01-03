98e51c898f660b3502af01d276997c4706f30312
jrynik <jrynik@yahoo.co.uk>
2012-01-03 19:44:35 +0000

    langues (paquet-compresseur)

---
+++ lang/paquet-compresseur_sk.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compresseur_description' => 'Kompresia css a javascriptu v hlavičke html stránok <code>ecrire/</code> a/lebo verejne prístupnej stránky',
+	'compresseur_slogan' => 'Kompresia css a javascriptu'
+);
+
+?>

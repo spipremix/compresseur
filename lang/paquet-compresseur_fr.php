bdb997db11a4403a34c0c6f1105c737cdf7869ec
salvatore <salvatore@rezo.net>
2012-01-03 12:10:15 +0000

    langues (paquet-compresseur)

--- lang/paquet-compresseur_fr.php
+++ lang/paquet-compresseur_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-compresseur
-// Langue: fr
-// Date: 30-07-2011 15:01:36
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/compresseur/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// C
+	// C
 	'compresseur_description' => 'Compression des css et javascript dans l\'entete des pages html de <code>ecrire/</code> et/ou du site public',
-	'compresseur_slogan' => 'Compression des css et javascript',
+	'compresseur_slogan' => 'Compression des css et javascript'
 );
+
 ?>

7f6e66308b4902ab1cad75010a12f721e6a83adf
davoodhossein <davoodhossein@yahoo.com>
2012-03-03 10:43:17 +0000

    langues (paquet-compresseur)

---
+++ lang/paquet-compresseur_fa.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org/tradlang_module/paquet-compresseur?lang_cible=fa
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compresseur_description' => 'Compression des css et javascript dans l\'en-tête des pages html de <code>ecrire/</code> et/ou du site public', # NEW
+	'compresseur_slogan' => 'فشرده سازي سي.اس.اس‌ها و جاوااسكريبت'
+);
+
+?>

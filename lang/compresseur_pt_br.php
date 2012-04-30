1ee5d32ed89c7f206a9d63837dd6b85cbe8572d4
rporto <rporto@calepino.com>
2012-04-30 08:28:07 +0000

    langues (compresseur)

--- lang/compresseur_pt_br.php
+++ lang/compresseur_pt_br.php
@@ -9,20 +9,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_compresseur_titre' => 'Otimizações e compressão',
-	'info_question_activer_compactage_css' => 'Você deseja ativar a compactação das folhas de estilo (CSS)?', # MODIF
-	'info_question_activer_compactage_js' => 'Você deseja ativar a compactação dos scripts (javascript)?', # MODIF
-	'info_question_activer_compresseur' => 'Você deseja ativar a compressão do fluxo HTTP?', # MODIF
-	'item_compresseur_closure' => 'Utiliser Google Closure Compiler [expérimental]', # NEW
-	'item_compresseur_css' => 'Activer la compression des feuilles de styles (CSS)', # NEW
-	'item_compresseur_html' => 'Activer la compression du HTML', # NEW
-	'item_compresseur_js' => 'Activer la compression des scripts (javascript)', # NEW
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Scripts',
+	'info_question_activer_compresseur' => 'HTML',
+	'item_compresseur_closure' => 'Usar o Google Closure Compiler [experimental]',
+	'item_compresseur_css' => 'Ativar a compressão das folhas de estilo (CSS)',
+	'item_compresseur_html' => 'Ativar a compressão do HTML',
+	'item_compresseur_js' => 'Ativar a compressão dos scripts (javascript)',
 
 	// T
 	'texte_compacter_avertissement' => 'Atenção para não ativar estas opções durante o desenvolvimento do seu site: os elementos compactados perdem toda a legibilidade.',
 	'texte_compacter_script_css' => 'O SPIP pode compactar os scripts javascript e as folhas de estilo CSS, para gravá-los nos arquivos de estatísticas; isto acelera a exibição do site.',
 	'texte_compresseur_page' => 'O SPIP pode comprimir automaticamente cada página que ele envia aos visitantes do site. Esta regulagem permite otimizar a banda de transmissão (le site torna-se mais rápido quando estiver sob uma conexão de banda baixa), mas demanda mais potência do servidor.',
 	'titre_compacter_script_css' => 'Compactação de scripts e CSS',
-	'titre_compresser_flux_http' => 'Compressão do fluxo HTTP' # MODIF
+	'titre_compresser_flux_http' => 'Compressão do HTML'
 );
 
 ?>

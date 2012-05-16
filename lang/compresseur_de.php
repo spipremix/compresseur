105e52ae7926c92e8374456d72a2c23d3389e98f
salvatore <salvatore@rezo.net>
2012-05-16 01:39:24 +0000

    langues (compresseur)

--- lang/compresseur_de.php
+++ lang/compresseur_de.php
@@ -9,13 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_compresseur_titre' => 'Optimierung und Kompression',
-	'info_question_activer_compactage_css' => 'Möchten Sie die Stylesheets (CSS) komprimieren?', # MODIF
-	'info_question_activer_compactage_js' => 'Möchten Sie die Javascripte komprimieren ?', # MODIF
-	'info_question_activer_compresseur' => 'Möchten sie die HTTP-Übertragung komprimieren?', # MODIF
-	'item_compresseur_closure' => 'Utiliser Google Closure Compiler [expérimental]', # NEW
-	'item_compresseur_css' => 'Activer la compression des feuilles de styles (CSS)', # NEW
-	'item_compresseur_html' => 'Activer la compression du HTML', # NEW
-	'item_compresseur_js' => 'Activer la compression des scripts (javascript)', # NEW
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Skripte',
+	'info_question_activer_compresseur' => 'HTML',
+	'item_compresseur_closure' => 'Google Closure Compiler verwenden [experimentell]',
+	'item_compresseur_css' => 'Kompression der Stylesheets aktivieren (CSS)',
+	'item_compresseur_html' => 'Kompression von HTML aktivieren',
+	'item_compresseur_js' => 'Kompression der Skripte aktivieren (Javascript)',
 
 	// T
 	'texte_compacter_avertissement' => 'Achtung: Bitte aktivieren Sie diese Option nicht während der Entwicklung Ihrer Site. Die komprimierten Elemente verlieren dadurch ihre Lesbarkeit.',
@@ -25,7 +25,7 @@ komprimieren.Diese Einstellung ermöglicht es, Bandbreite zu sparen (Die
 Site wird über langsame Verbindungen schneller angezeigt), benötigt
 jedoch zusätzliche Serverleistung.',
 	'titre_compacter_script_css' => 'Skripte und CSS komprimieren',
-	'titre_compresser_flux_http' => 'HTTP-Übertragung komprimieren' # MODIF
+	'titre_compresser_flux_http' => 'HTML komprimieren'
 );
 
 ?>

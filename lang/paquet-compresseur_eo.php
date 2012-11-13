b8298c76419e1b307f6be9ca2f8005620a20bb41
Matthieu Marcillaud <marcimat@rezo.net>
2012-11-13 02:41:49 +0000

    langues (paquet-compresseur)

---
+++ lang/paquet-compresseur_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-compresseur?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compresseur_description' => 'Compression des css et javascript dans l\'en-tête des pages html de <code>ecrire/</code> et/ou du site public', # NEW
+	'compresseur_slogan' => 'Densigo css kaj javascript'
+);
+
+?>

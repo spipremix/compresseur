fdbe776db42506f805a3f54418c0adbc5f5e4588
salvatore <salvatore@rezo.net>
2013-09-30 22:23:59 +0000

    langues (compresseur)

--- lang/compresseur_fr_tu.php
+++ lang/compresseur_fr_tu.php
@@ -9,9 +9,13 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// I
 	'info_compresseur_titre' => 'Optimisations et compression',
-	'info_question_activer_compactage_css' => 'Souhaite-tu activer le compactage des feuilles de style (CSS) ?', # MODIF
-	'info_question_activer_compactage_js' => 'Souhaite-tu activer le compactage des scripts (javascript) ?', # MODIF
-	'info_question_activer_compresseur' => 'Veux-tu activer la compression du flux HTTP ?', # MODIF
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Scripts',
+	'info_question_activer_compresseur' => 'HTML',
+	'item_compresseur_closure' => 'Utiliser Google Closure Compiler [expérimental]',
+	'item_compresseur_css' => 'Activer la compression des feuilles de style (CSS)',
+	'item_compresseur_html' => 'Activer la compression du HTML',
+	'item_compresseur_js' => 'Activer la compression des scripts (javascript)',
 
 	// T
 	'texte_compacter_avertissement' => 'Attention à ne pas activer ces options durant le développement de ton site : les éléments compactés perdent toute lisibilité.',
@@ -21,7 +25,7 @@ visiteurs du site. Ce réglage permet d’optimiser la bande passante (le
 site est plus rapide derrière une liaison à faible débit), mais
 demande plus de puissance au serveur.',
 	'titre_compacter_script_css' => 'Compactage des scripts et CSS',
-	'titre_compresser_flux_http' => 'Compression du flux HTTP' # MODIF
+	'titre_compresser_flux_http' => 'Compression du HTML'
 );
 
 ?>

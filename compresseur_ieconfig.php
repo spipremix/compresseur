2de7b1547325ddba93b4c4f2041b48a1532f4001
Bruno Bergot <bruno@eliaz.fr>
2013-11-30 15:16:18 +0000

    Version 1.9.0 Fix de http://core.spip.org/issues/3076 : retrait de la fonctionnalité de compression du HTML dans la page de configuration avancée, on garde la fonction minifier_html() et sa lib pour ne pas péter les scripts qui l'utiliseraient.

--- compresseur_ieconfig.php
+++ compresseur_ieconfig.php
@@ -20,7 +20,7 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 function compresseur_ieconfig_metas($table){
 	$table['compresseur']['titre'] = _T('compresseur:info_compresseur_titre');
 	$table['compresseur']['icone'] = 'compresseur-16.png';
-	$table['compresseur']['metas_brutes'] = 'auto_compress_http,auto_compress_js,auto_compress_css,auto_compress_closure';
+	$table['compresseur']['metas_brutes'] = 'auto_compress_js,auto_compress_css,auto_compress_closure';
 	
 	return $table;
 }
--- formulaires/configurer_compresseur.php
+++ formulaires/configurer_compresseur.php
@@ -27,8 +27,6 @@ function formulaires_configurer_compresseur_charger_dist(){
 
 	$valeurs = array();
 
-	$valeurs['_editer_auto_compress_http'] = function_exists('ob_gzhandler');
-	$valeurs['auto_compress_http'] = $GLOBALS['meta']['auto_compress_http'];
 	$valeurs['auto_compress_js'] = $GLOBALS['meta']['auto_compress_js'];
 	$valeurs['auto_compress_css'] = $GLOBALS['meta']['auto_compress_css'];
 	$valeurs['auto_compress_closure'] = $GLOBALS['meta']['auto_compress_closure'];
@@ -47,10 +45,10 @@ function formulaires_configurer_compresseur_verifier_dist(){
 	$erreurs = array();
 	
 	// les checkbox
-	foreach(array('auto_compress_http','auto_compress_js','auto_compress_css', 'auto_compress_closure') as $champ)
+	foreach(array('auto_compress_js','auto_compress_css', 'auto_compress_closure') as $champ)
 		if (_request($champ)!='oui')
 			set_request($champ,'non');
-			
+		
 	return $erreurs;
 }
 
@@ -63,7 +61,7 @@ function formulaires_configurer_compresseur_verifier_dist(){
 function formulaires_configurer_compresseur_traiter_dist(){
 	include_spip('inc/config');
 	appliquer_modifs_config();
-		
+	
 	return array('message_ok'=>_T('config_info_enregistree'));
 }
 

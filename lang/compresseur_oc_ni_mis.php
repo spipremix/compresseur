c5b2544f8da0ccbb346a9ba5309d7c8598758ca3
spip <spip@oliver-fr.org>
2017-03-01 04:33:01 +0000

    [Salvatore] [source:_core_/plugins/compresseur/lang/ compresseur] Export depuis http://trad.spip.net de la langue oc_ni_mis

--- lang/compresseur_oc_ni_mis.php
+++ lang/compresseur_oc_ni_mis.php
@@ -17,6 +17,9 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'item_compresseur_css' => 'Ativà la coumpressioun dei fuèi de stil (CSS)',
 	'item_compresseur_js' => 'Ativà la coumpressioun dei script (javascript)',
 
+	// L
+	'label_url_statique_ressources' => 'URL ressórça',
+
 	// T
 	'texte_compacter_avertissement' => 'Mèfi da noun ativà aqueli oupcioun dau tems dóu desfouloupamen dóu vouòstre sit : lu elemen serat perdon touta legibilità.',
 	'texte_compacter_script_css' => 'SPIP pòu serà lu script javascript e li fuèi de stil CSS, per li registrà en de fichié estàticou ; acò achelerà l’afichage dóu sit.',

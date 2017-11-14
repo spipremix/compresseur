2585d7b5bb0234d1c3ef2edb315f39e990ac07a8
xuacusk8 <xuacusk8@gmail.com>
2017-11-14 17:43:37 +0000

    [Salvatore] [source:_core_/plugins/compresseur/lang/ compresseur] Export depuis http://trad.spip.net de la langue ast

--- lang/compresseur_ast.php
+++ lang/compresseur_ast.php
@@ -9,13 +9,21 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// E
+	'explication_url_statique_ressources' => 'Una URL que permite sirvir los ficheros estáticos (imáxenes, JS…) ensin cookie. Polo xeneral, un sub-dominiu alternativu que tamién apunta al sitiu.',
+
 	// I
 	'info_compresseur_titre' => 'Igües y compresión',
-	'info_question_activer_compactage_css' => '¿Quies activar la compautación de les fueyes d’estilu (CSS)?', # MODIF
-	'info_question_activer_compactage_js' => '¿Quies activar la compautación de los scripts (JavaScript)?', # MODIF
+	'info_question_activer_compactage_css' => 'CSS',
+	'info_question_activer_compactage_js' => 'Scripts',
+	'item_compresseur_css' => 'Activar la comprensión de les fueyes d’estilu (CSS)',
+	'item_compresseur_js' => 'Activar la compresión de los scripts (javascript)',
+
+	// L
+	'label_url_statique_ressources' => 'Recursos URL',
 
 	// T
-	'texte_compacter_avertissement' => 'Cuida de nun activar estes opciones durante el desarrollo del sitiu: los elementos compactaos dexen dafechu de ser lleibles.',
+	'texte_compacter_avertissement' => 'Cuida de nun activar estes opciones mientres se desendolca’l sitiu: los elementos compautaos pierden la visibilidá.',
 	'texte_compacter_script_css' => 'SPIP pue compautar los scripts JavaScript y les fueyes d’estilu CSS, pa guardalos como archivos estáticos; esto acelera l’amosamientu del sitiu.',
 	'titre_compacter_script_css' => 'Compautación de los scripts y CSS'
 );

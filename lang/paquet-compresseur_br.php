463fcf472845cfc7f925218d1616d1dd9325f5c4
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-10 04:36:34 +0000

    [Salvatore] [source:_core_/plugins/compresseur/lang/ paquet-compresseur] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-compresseur_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-compresseur?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compresseur_description' => 'Gwaskadurez css ha javascript en titouroù ar pajennoù html deus <code>ecrire/</code> ha/pe deus al lec’h publik',
+	'compresseur_slogan' => 'Gwaskadurezh css ha javascript'
+);

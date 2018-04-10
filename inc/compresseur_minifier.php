67d51e001f56303165460712f3a9696861e59994
tcharlss <tcharlss@bravecassine.com>
2018-04-10 10:02:08 +0000

    Ticket #3991 : préserver les ressources encodées en base64. Lorsqu'une valeur est égale à zéro (ex 0px), s'assurer qu'il s'agit d'une unité avant de retirer celle-ci (une chaîne encodée en base64 peut également contenir des parties '0px')

--- inc/compresseur_minifier.php
+++ inc/compresseur_minifier.php
@@ -97,8 +97,8 @@ function minifier_css($contenu, $options = '') {
 		$contenu = preg_replace("/\b0+\.(\d+em)/ims", ".$1", $contenu);
 		// supprimer les declarations vides
 		$contenu = preg_replace(",(^|})([^{}]*){},Ums", "$1", $contenu);
-		// zero est zero, quelle que soit l'unite (sauf pour % car casse les @keyframes cf https://core.spip.net/issues/3128)
-		$contenu = preg_replace("/([^0-9.]0)(em|px|pt)/ms", "$1", $contenu);
+		// pas besoin d'unité quand la valeur est zéro (sauf pour % car casse les @keyframes cf https://core.spip.net/issues/3128 - préserve également les chaînes en base64 cf https://core.spip.net/issues/3991)
+		$contenu = preg_replace("/((?::|\s+|\()0)(em|px|pt|rem|ex|pc|vh|vw|vmin|vmax|cm|mm|in|ch)/ms", "$1", $contenu);
 
 		// renommer les couleurs par leurs versions courtes quand c'est possible
 		$colors = array(

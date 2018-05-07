f03e75b3f191a38aedfbf75f55e8e6366c5034e1
tcharlss <tcharlss@bravecassine.com>
2018-05-07 19:43:04 +0000

    Oups coquille dans r109864 (merci jluc)

--- inc/compresseur_minifier.php
+++ inc/compresseur_minifier.php
@@ -97,8 +97,8 @@ function minifier_css($contenu, $options = '') {
 		$contenu = preg_replace("/\b0+\.(\d+em)/ims", ".$1", $contenu);
 		// supprimer les declarations vides
 		$contenu = preg_replace(",(^|})([^{}]*){},Ums", "$1", $contenu);
-		// pas besoin d'unité quand la valeur est zéro (sauf pour % car casse les @keyframes cf https://core.spip.net/issues/3128 - préserve également les chaînes en base64 cf https://core.spip.net/issues/3991)
-		$contenu = preg_replace("/\b0(em|px|pt|rem|ex|pc|vh|vw|vmin|vmax|cm|mm|in|ch)\b/ms", "$1", $contenu);
+		// supprimer l'unité quand la valeur est zéro (sauf pour % car casse les @keyframes cf https://core.spip.net/issues/3128 et sauf pour les chaînes en base64 cf https://core.spip.net/issues/3991)
+		$contenu = preg_replace("/\b0(em|px|pt|rem|ex|pc|vh|vw|vmin|vmax|cm|mm|in|ch)\b/ms", '0', $contenu);
 
 		// renommer les couleurs par leurs versions courtes quand c'est possible
 		$colors = array(

435bb32f0cd4b05ef9befbea852029cb2d3f4590
Matthieu Marcillaud <marcimat@rezo.net>
2018-09-05 11:19:12 +0000

    Tickets #4059 et #4138 : meilleure compat PHP 7.2
    - create_function en moins.

--- inc/compresseur_embarquer.php
+++ inc/compresseur_embarquer.php
@@ -47,10 +47,9 @@ function compresseur_embarquer_images_css($contenu, $source, $source_file = null
 
 	return preg_replace_callback(
 		",url\s*\(\s*['\"]?([^'\"/][^:]*[.](png|gif|jpg))['\"]?\s*\),Uims",
-		create_function(
-			'$x',
-			'return "url(\"".' . $filtre_embarque_fichier . '($x[1],"' . $base . '",_CSS_EMBARQUE_FICHIER_MAX_SIZE)."\")";'
-		),
+		function($x) use ($filtre_embarque_fichier, $base) {
+			return "url(\"" . $filtre_embarque_fichier($x[1], $base, _CSS_EMBARQUE_FICHIER_MAX_SIZE) . "\");";
+		},
 		$contenu
 	);
 }

08df45dfab9ed9f68c9ee08b3f83edc222bba622
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-26 03:39:36 +0000

    [Salvatore] [source:_core_/plugins/compresseur/lang/ paquet-compresseur] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/paquet-compresseur_ja.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-compresseur?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compresseur_description' => '<code>ecrire/</code>と言うhtmlページや公開サイトのヘッダーにあるCSSとjavascriptの圧縮',
+	'compresseur_slogan' => 'CSSとjavascriptの圧縮'
+);
